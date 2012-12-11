e21a08d9ac5259536453df8c407cc22810f52e1c
unknown <Siddharth@MURCHINGDELL.(none)>
2012-12-10 20:33:58 -0800

    Deleting more unnecessary migration files

--- db/migrate/20121206042454_remake_reset_codein_users.rb
+++ db/migrate/20121206042454_remake_reset_codein_users.rb
@@ -1,6 +1,5 @@
 class RemakeResetCodeinUsers < ActiveRecord::Migration
   def up
-    remove_column :users, :reset_code
     add_column :users, :reset_code, :string
     add_column :users, :reset_code_timestamp, :datetime
     
@@ -8,7 +7,6 @@ class RemakeResetCodeinUsers < ActiveRecord::Migration
 
   def down
     remove_column :users, :reset_code
-    add_column :users, :reset_code, :text
     remove_column :users, :reset_code_timestamp
   end
 end
