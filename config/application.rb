78e8fbfd2d48a21cfb2b30f2384074831cf9685e
Siddharth Murching <smurching@gmail.com>
2012-12-23 01:54:28 -0800

    changed config file so hopefully heroku will precompile applications.js properly

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,8 @@ module Ideagarden
     
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Heroku requires the line below:
+    config.assets.initialize_on_precompile = false
   end
 end
