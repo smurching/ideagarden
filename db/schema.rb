691de205db0ec317af84ccd4ca1a81a04b5fa2b4
Siddharth Murching <smurching@gmail.com>
2013-06-28 12:31:48 -0700

    debugging registration process on heroku by readding confirm code column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130628013317) do
+ActiveRecord::Schema.define(:version => 20130628182904) do
 
   create_table "confirmcodes", :force => true do |t|
     t.string   "code"
